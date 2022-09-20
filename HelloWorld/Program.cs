66e3ba1b3c03e11756c061a58cfe873c9c5ac7ce
Daruyami <shadedgreen65@gmail.com>
2022-09-20 12:51:09 +0200

    Ponowne dodanie projektów do solucji

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello World!");
+        }
+    }
+}
